58e15cff168c0108ce06db83c8043876e28cc356
Simon Rofe <simon.rofe@ieee.org>
2022-08-08 17:07:27 +1000

    Initial Commit

---
+++ TimeSheets/TimeSheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeSheetsApp.swift
+//  TimeSheets
+//
+//  Created by Simon Rofe on 8/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeSheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
